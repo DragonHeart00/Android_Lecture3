9a4f8e44649ab529a24d007cd63f845253fde808
Sulaiman Kasas <s195462@student.dtu.dk>
2020-09-17 20:05:23 +0200

    er i gang med Fragment5

--- app/src/main/java/com/solo/myfirstandroidapp/ui/Fragmentexercises/Fragment5.java
+++ app/src/main/java/com/solo/myfirstandroidapp/ui/Fragmentexercises/Fragment5.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,14 +13,29 @@ import androidx.fragment.app.Fragment;
 import com.solo.myfirstandroidapp.R;
 
 public class Fragment5 extends Fragment {
-
+    private Button implicitte, eksplicitte;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment5_layout,container,false);
+        implicitte = view.findViewById(R.id.implicitte);
+        eksplicitte = view.findViewById(R.id.eksplicitte);
+
+        implicitte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
+        eksplicitte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         return view;
     }
